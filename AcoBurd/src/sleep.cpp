e569a6602b456e95742fa30e82263b9172ce3e2c
Cooper Mattern <cnamattern@gmail.com>
2025-01-18 20:18:21 +0000

    if-statement and other formatting in sleep.cpp

--- AcoBurd/src/sleep.cpp
+++ AcoBurd/src/sleep.cpp
@@ -56,11 +56,14 @@ int get_wait_screen_delay(void){
 }
 
 void init_sleep(){
-  TimerInit( &wakeUp, TimerWakeUp );
+  TimerInit(&wakeUp, TimerWakeUp);
 }
 
 void TimerWakeUp(){
-  if (debug) Serial.printf("Woke up at %ld s.  Countdown timer %ld.\n", InternalClock(), get_release_timer());
+  if(debug){
+    Serial.printf("Woke up at %ld s.  Countdown timer %ld.\n", InternalClock(), get_release_timer());
+  }
+
   low_power = false;
   feedInnerWdt();           // Pet the watchdog
 
@@ -70,52 +73,65 @@ void TimerWakeUp(){
   set_motor_state();                                                                                    // This opens and closes the release based on time
 
   // Flash LED or turn it off if not needed
-  if (led_enable) {
-    if ( (get_waiting_to_be_retrieved() == 1) && (get_display_active() == 0) ) led_flasher();
-    else rgb_led(0, 0, 0); // Set all pixel colors to 'off'
+  if(led_enable){
+    if((get_waiting_to_be_retrieved() == 1) && (get_display_active() == 0)){
+      led_flasher();
+    }
+    else{
+      // Set all pixel colors to 'off'
+      rgb_led(0, 0, 0);
+    } 
   }
-
 }
 
 void go_to_sleep(){
   // Prevent CPU sleep if GPS is active
-  if (get_gps_enabled() == 1) set_sleep_inhibit(1);
-  else set_sleep_inhibit(0);
+  if (get_gps_enabled() == 1){
+    set_sleep_inhibit(1);
+  }
+  else{
+    set_sleep_inhibit(0);
+  }
 
-  if (!get_sleep_inhibit()) low_power = true;
-  if (get_sleep_inhibit()) low_power = false;
+  if(!get_sleep_inhibit()){
+    low_power = true;
+  }
+
+  if(get_sleep_inhibit()){
+    low_power = false;
+  }
 
   set_display_active(0);
   motor_sleep();
   oled_sleep();
 
-  if ( (get_waiting_to_be_retrieved == 0) ){
+  if((get_waiting_to_be_retrieved == 0)){
     VextOFF();                  // Turn off Vext only if release is not waiting, otherwise supply power to LED
+  }
 
-  } 
   // If allowed to sleep, then set timer
-  if (!get_sleep_inhibit()) {
+  if(!get_sleep_inhibit()){
     Radio.Sleep( );                                                 // Power down radio if sleep is allowed
     TimerSetValue( &wakeUp, timetillwakeup );
     TimerStart( &wakeUp );
   }
-  else {
+  else{
     delay(timetillwakeup);                                          // Just wait around for a while if sleep is inhibited
     TimerWakeUp();
   }
-
 }
 
 // Reed switch interrupt
-void gpio_interrupt() {
+void gpio_interrupt(){
   low_power = false;
   set_reed_switch1(!digitalRead(reed_switch_input1));                                                          // Signal is inverted
   set_reed_switch2(!digitalRead(reed_switch_input2));                                                          // Signal is inverted
 
   noInterrupts();                                                                                         // Disable interrupts or motor won't spin
-  if (debug){
+  if(debug){
     Serial.printf("GPIO Interrupt at %d ms.\n", InternalClock() );
   }
+
   reed_switch_first_press = InternalClock();                                                              // new press, record first time of key press
   am_i_waiting_to_be_recovered();
   set_display_timer(InternalClock() + display_timeout);                                                      // Activate display
@@ -123,6 +139,6 @@ void gpio_interrupt() {
   interrupts();
 }
 
-bool is_low_power() {
+bool is_low_power(){
   return low_power;
 }
