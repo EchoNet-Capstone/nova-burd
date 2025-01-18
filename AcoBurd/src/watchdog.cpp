1780341ea3fe8a6e3c9554bfffbc29eefafd2dd1
Cooper Mattern <cnamattern@gmail.com>
2025-01-18 19:56:13 +0000

    removing whitespace and formatting watchdog.cpp

--- AcoBurd/src/watchdog.cpp
+++ AcoBurd/src/watchdog.cpp
@@ -5,9 +5,7 @@
 #include <cyfitter.h>
 #include <cydevice_trm.h>
 
-
-void wdt_isr_Start(void)
-{
+void wdt_isr_Start(void){
     /* For all we know the interrupt is active. */
     wdt_isr_Disable();
 
@@ -21,9 +19,7 @@ void wdt_isr_Start(void)
     wdt_isr_Enable();
 }
 
-
-void wdt_isr_StartEx(cyisraddress address)
-{
+void wdt_isr_StartEx(cyisraddress address){
     /* For all we know the interrupt is active. */
     wdt_isr_Disable();
 
@@ -37,9 +33,7 @@ void wdt_isr_StartEx(cyisraddress address)
     wdt_isr_Enable();
 }
 
-
-void wdt_isr_Stop(void)
-{
+void wdt_isr_Stop(void){
     /* Disable this interrupt. */
     wdt_isr_Disable();
 
@@ -47,9 +41,7 @@ void wdt_isr_Stop(void)
     wdt_isr_SetVector(&IntDefaultHandler);
 }
 
-
-CY_ISR(wdt_isr_Interrupt)
-{
+CY_ISR(wdt_isr_Interrupt){
     #ifdef wdt_isr_INTERRUPT_INTERRUPT_CALLBACK
         wdt_isr_Interrupt_InterruptCallback();
     #endif /* wdt_isr_INTERRUPT_INTERRUPT_CALLBACK */ 
@@ -60,20 +52,15 @@ CY_ISR(wdt_isr_Interrupt)
     /* `#END` */
 }
 
-void wdt_isr_SetVector(cyisraddress address)
-{
+void wdt_isr_SetVector(cyisraddress address){
     CyRamVectors[CYINT_IRQ_BASE + wdt_isr__INTC_NUMBER] = address;
 }
 
-
-cyisraddress wdt_isr_GetVector(void)
-{
+cyisraddress wdt_isr_GetVector(void){
     return CyRamVectors[CYINT_IRQ_BASE + wdt_isr__INTC_NUMBER];
 }
 
-
-void wdt_isr_SetPriority(uint8 priority)
-{
+void wdt_isr_SetPriority(uint8 priority){
   uint8 interruptState;
     uint32 priorityOffset = ((wdt_isr__INTC_NUMBER % 4u) * 8u) + 6u;
     
@@ -83,9 +70,7 @@ void wdt_isr_SetPriority(uint8 priority)
   CyExitCriticalSection(interruptState);
 }
 
-
-uint8 wdt_isr_GetPriority(void)
-{
+uint8 wdt_isr_GetPriority(void){
     uint32 priority;
   uint32 priorityOffset = ((wdt_isr__INTC_NUMBER % 4u) * 8u) + 6u;
 
@@ -94,47 +79,35 @@ uint8 wdt_isr_GetPriority(void)
     return (uint8)priority;
 }
 
-
-void wdt_isr_Enable(void)
-{
+void wdt_isr_Enable(void){
     /* Enable the general interrupt. */
     *wdt_isr_INTC_SET_EN = wdt_isr__INTC_MASK;
 }
 
-
-uint8 wdt_isr_GetState(void)
-{
+uint8 wdt_isr_GetState(void){
     /* Get the state of the general interrupt. */
     return ((*wdt_isr_INTC_SET_EN & (uint32)wdt_isr__INTC_MASK) != 0u) ? 1u:0u;
 }
 
-
-void wdt_isr_Disable(void)
-{
+void wdt_isr_Disable(void){
     /* Disable the general interrupt. */
     *wdt_isr_INTC_CLR_EN = wdt_isr__INTC_MASK;
 }
 
-
-void wdt_isr_SetPending(void)
-{
+void wdt_isr_SetPending(void){
     *wdt_isr_INTC_SET_PD = wdt_isr__INTC_MASK;
 }
 
-
-void wdt_isr_ClearPending(void)
-{
+void wdt_isr_ClearPending(void){
     *wdt_isr_INTC_CLR_PD = wdt_isr__INTC_MASK;
 }
 
-void feedInnerWdt()
-{
+void feedInnerWdt(){
   CySysWdtClearInterrupt();
   wdt_isr_ClearPending();
 }
 
-void innerWdtEnable(bool feed)
-{
+void innerWdtEnable(bool feed){
     /* Start the WDT */
     CySysWdtEnable();
     CySysWdtWriteMatch(0xFFFF);
