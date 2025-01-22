c290a7bb11f18f4021ba31e57c6f8a3649f370e5
Cooper Mattern <cnamattern@gmail.com>
2025-01-22 18:30:06 -0500

    Revert "Build"

--- burd-old/Heltec_LowPower_Release_R47_Hall_Effect/display.h
+++ burd-old/Heltec_LowPower_Release_R47_Hall_Effect/display.h
@@ -1,60 +1,51 @@
-#include "display.hpp"
-#include "subroutines.hpp"
-#include "globals.hpp"
-#include "HT_SSD1306Wire.h"
-#include "CubeCell_NeoPixel.h"
-#include "sleep.hpp"
-
-SSD1306Wire  oled(0x3c, 500000, SDA, SCL, GEOMETRY_128_64, GPIO10); // addr , freq , SDA, SCL, resolution , rst
-
-// Configure RGB LED
-CubeCell_NeoPixel rgbpixel(1, RGB, NEO_GRB + NEO_KHZ800);
-
 // Sleep display for power saving
-void oled_sleep(){
+void oled_sleep() {
   oled.sleep();//OLED sleep
   //VextOFF();
 }
 
+
 // Wake up display
-void oled_wake(){
+void oled_wake() {
   VextON();
   oled.wakeup();
 }
 
-void update_display(){
-  // Time until release
-  set_time_until_release(get_release_timer() - InternalClock());
-  
-  if(get_time_until_release() < 0){
-    set_time_until_release(0);
-  }
 
-  int release_days = get_time_until_release() / 86400;
-  int release_hours = (get_time_until_release() - (release_days * 86400) ) / 3600;
-  int release_minutes = (get_time_until_release() - (release_days * 86400) - (release_hours * 3600) ) / 60;
-  int release_seconds = (get_time_until_release() - (release_days * 86400) - (release_hours * 3600) - (release_minutes * 60) );
+void update_display() {
+  //long time_until_release;
+
+  // Time until release
+  time_until_release = release_timer - InternalClock();
+  if (time_until_release < 0) time_until_release = 0;
+
+  int release_days = time_until_release / 86400;
+  int release_hours = (time_until_release - (release_days * 86400) ) / 3600;
+  int release_minutes = (time_until_release - (release_days * 86400) - (release_hours * 3600) ) / 60;
+  int release_seconds = (time_until_release - (release_days * 86400) - (release_hours * 3600) - (release_minutes * 60) );
 
   // 9 pixel height works well for line spacing
   oled_wake();
   oled.clear();
   oled.setFont(ArialMT_Plain_10);
-  oled.drawString(0, 0, "Battery: " + (String)get_battery_percent() + "%");
+  oled.drawString(0, 0, "Battery: " + (String)battery_percent + "%");
+  //oled.drawString(10, 0, "Battery: " + (String)(battery_volts) + "mV  " + (String)battery_percent + "%");
 
-  if(get_gps_lock() == 1){
+  if (gps_lock == 1) {
     oled.drawString(85, 0, "GPS Lock");
   }
-  else if(get_gps_enabled() == 1){
+  else if (gps_enabled == 1) {
     oled.drawString(85, 0, "GPS On");
   }
-  else{
+  else {
     oled.drawString(85, 0, "GPS Off");
   }
 
-  if(get_time_until_release > 0){
+
+  if (time_until_release > 0) {
     int reset_countdown = 0;
 
-    if (get_reed_switch1() || get_reed_switch2()  ) reset_countdown = reed_switch_long_press - (InternalClock() - get_reed_switch_first_press());
+    if (reed_switch1 || reed_switch2 ) reset_countdown = reed_switch_long_press - (InternalClock() - reed_switch_first_press);
     else reset_countdown = reed_switch_long_press;
 
     oled.drawString(5, 15, "Hold magnet " + (String)reset_countdown + " seconds");
@@ -66,45 +57,45 @@ void update_display(){
     oled.setFont(ArialMT_Plain_16);
 
     // Draw Days
-    if(release_days < 10){
+    if (release_days < 10) {
       oled.drawString(0, 40, "  " + (String)release_days);
       oled.drawString(1, 40, "  " + (String)release_days);
     }
-    else if(release_days < 100){
+    else if (release_days < 100) {
       oled.drawString(0, 40, " " + (String)release_days);
       oled.drawString(1, 40, " " + (String)release_days);
     }
-    else{
+    else {
       oled.drawString(0, 40, (String)release_days);
       oled.drawString(1, 40, (String)release_days);
     }
 
     // Draw Hours
-    if(release_hours < 10){
+    if (release_hours < 10) {
       oled.drawString(42, 40, " " + (String)release_hours);
       oled.drawString(43, 40, " " + (String)release_hours);
     }
-    else{
+    else {
       oled.drawString(42, 40, (String)release_hours);
       oled.drawString(43, 40, (String)release_hours);
     }
 
     // Draw Minutes
-    if(release_minutes < 10){
+    if (release_minutes < 10) {
       oled.drawString(74, 40, "0" + (String)release_minutes);
       oled.drawString(75, 40, "0" + (String)release_minutes);
     }
-    else{
+    else {
       oled.drawString(74, 40, (String)release_minutes);
       oled.drawString(75, 40, (String)release_minutes);
     }
 
     // Draw Seconds
-    if(release_seconds < 10){
+    if (release_seconds < 10) {
       oled.drawString(107, 40, "0" + (String)release_seconds);
       oled.drawString(108, 40, "0" + (String)release_seconds);
     }
-    else{
+    else {
       oled.drawString(107, 40, (String)release_seconds);
       oled.drawString(108, 40, (String)release_seconds);
     }
@@ -112,7 +103,8 @@ void update_display(){
     oled.setFont(ArialMT_Plain_10);                                       // Reset font back to small
     oled.drawString(0, 54, "DAYS    HRS    MIN     SEC");
   }
-  else{
+
+  else {
     oled.drawString(8, 16, "Hold magnet for at least ");
     oled.drawString(12, 25, (String)reed_switch_short_press + " second to set timer.");
 
@@ -121,31 +113,37 @@ void update_display(){
     oled.drawString(1, 45, "RELEASE OPEN");
   }
 
+
   oled.setFont(ArialMT_Plain_10);                                       // Reset font back to small
   // Is release open?
   //if ( release_is_open == 1 ) oled.drawString(20, 54, "RELEASE OPEN");
   //else oled.drawString(15, 54, "RELEASE CLOSED");
 
   oled.display();
+
 }
 
-void rgb_led(uint8_t red, uint8_t green, uint8_t blue){
-  if((red > 0) || (green > 0) || (blue > 0)){
+
+void rgb_led(uint8_t red, uint8_t green, uint8_t blue) {
+  if ( (red > 0) || (green > 0) || (blue > 0) ) {
     //VextON();
     rgbpixel.begin(); // INITIALIZE RGB strip object (REQUIRED)
     rgbpixel.clear(); // Set all pixel colors to 'off'
     rgbpixel.setPixelColor(0, rgbpixel.Color(red, green, blue));
     rgbpixel.show();   // Send the updated pixel colors to the hardware.
   }
-  else{
+  else {
     rgbpixel.begin(); // INITIALIZE RGB strip object (REQUIRED)
     rgbpixel.setPixelColor(0, rgbpixel.Color(red, green, blue));
     //rgbpixel.clear(); // Set all pixel colors to 'off'
     rgbpixel.show();   // Send the updated pixel colors to the hardware.
   }
+
 }
 
-void led_flasher(){
+
+void led_flasher() {
+
   rgb_led(255, 0, 0);
   delay(20);
   rgb_led(0, 0, 0);
@@ -153,15 +151,17 @@ void led_flasher(){
   rgb_led(0, 255, 0);
   delay(20);
   rgb_led(0, 0, 0);
-  set_is_led_activated(1);
+  is_led_activated = 1;
 
   //if (gps_lock == 1){
   //  rgb_led(0, 0, 32);
   //}
+
 }
 
+
 // Bootup company logo
-void logo(){
+void logo() {
   VextON();// oled power on;
   delay(10);
 
@@ -179,8 +179,10 @@ void logo(){
   //oled.setFont(ArialMT_Plain_10);
 }
 
+
+
 // Waiting screen
-void waiting_screen(){
+void waiting_screen() {
   VextON();// oled power on;
   delay(10);
 
