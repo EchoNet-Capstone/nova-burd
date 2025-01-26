677a51b23058decc118f668aaa84106caa1ca1d4
bobjoe400 <cnamattern@gmail.com>
2025-01-25 16:53:34 -0800

    whitespace

--- AcoBurd/include/get_set_macros.h
+++ AcoBurd/include/get_set_macros.h
@@ -6,12 +6,12 @@
 #define GETTER_SIG(x, y) x get_ ## y(void)
 
 
-#define GET_SET_FUNC_PROTO(x, y) SETTER_SIG(x, y); \
+#define GET_SET_FUNC_PROTO(x, y) SETTER_SIG(x, y);\
                              GETTER_SIG(x, y)
 
 #define GET_SET_DEF(x, y, z)  x y = z; \
                             SETTER_SIG(x, y){\
-                            y = SETTER_NEW(y); \
+                            y = SETTER_NEW(y);\
                             }\
                             GETTER_SIG(x, y){\
                             return y;\
