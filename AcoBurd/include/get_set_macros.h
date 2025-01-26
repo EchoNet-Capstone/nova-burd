1e89ce0b7cb6aa827a1c4e4912c3d5af500ce609
bobjoe400 <cnamattern@gmail.com>
2025-01-25 17:04:02 -0800

    more comments to get/set macros

--- AcoBurd/include/get_set_macros.h
+++ AcoBurd/include/get_set_macros.h
@@ -6,7 +6,7 @@
 #define GETTER_SIG(x, y) x get_ ## y(void)
 
 /**
- * For (var_type, var_name), this becomes:
+ * For (`var_type`, `var_name`), this becomes:
  * `void set_var_name(var_type new_var_name);` and
  * `var_type get_var_name(void);`
  */
@@ -14,7 +14,7 @@
                              GETTER_SIG(x, y)
 
 /**
- * For (var_type, var_name, var_inital_value), this becomes:
+ * For (`var_type`, `var_name`, `var_inital_value`), this becomes:
  * `var_type var_name = var_inital_value` and
  * `void set_var_name(var_type new_var_name){ var_name = new_var_name ;}` and
  * `var_type get_var_name(void){ return var_name; }`
