164ca18d07d0fedc8b81e92cd52b9019deee3e5d
EchoNet-Capstone <echonetcapstone@gmail.com>
2025-05-07 04:29:23 -0700

    oh this can go over here too

--- AcoBurd/lib/BurdLib/src/burd_EEPROM.cpp
+++ AcoBurd/lib/BurdLib/src/burd_EEPROM.cpp
@@ -4,6 +4,23 @@
 
 #include "burd_EEPROM.hpp"
 
+const uint8_t SERIAL_NUM_SIZE       = sizeof(uint32_t);
+
+const uint8_t MAGIC_SIZE            = sizeof(uint8_t);
+
+const uint8_t DEVICE_ID_SIZE        = sizeof(uint16_t);
+const uint8_t NETWORK_ID_SIZE       = sizeof(uint16_t);
+const uint8_t DID_NID_SET_SIZE      = sizeof(uint8_t);
+
+const uint8_t SERIAL_NUM_ADDR       = 0;
+
+const uint8_t EEPROM_MAGIC          = 0x42;
+const uint8_t MAGIC_ADDR            = SERIAL_NUM_ADDR + SERIAL_NUM_SIZE;
+
+const uint8_t DEVICE_ID_ADDR        = MAGIC_ADDR + MAGIC_SIZE;
+const uint8_t NETWORK_ID_ADDR       = DEVICE_ID_ADDR + NETWORK_ID_SIZE;
+const uint8_t DID_NID_SET_BYTE_ADDR = NETWORK_ID_ADDR + DID_NID_SET_SIZE;
+
 void
 EEPROM_firstTime(
     void
