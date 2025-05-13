738957ac9ba62dd4d215cdcb439f0602954fca77
ninastjohn <ninastjohn7@gmail.com>
2025-05-12 23:43:51 -0700

    BETTER BLOOM

--- AcoBurd/lib/BurdLib/include/bloomfilter.hpp
+++ AcoBurd/lib/BurdLib/include/bloomfilter.hpp
@@ -2,5 +2,29 @@
 
 #include <bits/stdc++.h>
 
-bool bloom_check(uint32_t key);
-bool bloom_check(uint32_t key);
+
+
+uint32_t 
+hash_packet_buffer(
+    uint8_t* buf, 
+    uint8_t size
+);
+
+
+
+bool 
+bloom_check_packet(
+    uint8_t* packetBuffer,
+     uint8_t size
+);
+
+
+void bloom_add_packet(
+    uint8_t* packetBuffer,
+    uint8_t size
+);
+
+void 
+bloom_add(
+    uint32_t key
+);
--- AcoBurd/lib/BurdLib/src/bloomfilter.cpp
+++ AcoBurd/lib/BurdLib/src/bloomfilter.cpp
@@ -6,36 +6,64 @@
 #include "bloomfilter.hpp"
 #include <bits/stdc++.h>
 
-#define BLOOM_FILTER_SIZE 8 // 64 bits
-static uint8_t bloom_filter[BLOOM_FILTER_SIZE];
+#define BLOOM_FILTER_BITS 64
+#define BLOOM_FILTER_BYTES (BLOOM_FILTER_BITS / 8)
 
-uint8_t hash1(uint32_t key);
-uint8_t hash2(uint32_t key);
+static uint8_t bloom_filter[BLOOM_FILTER_BYTES] = {0};
 
 uint8_t 
-hash1(uint32_t key)
- {
+hash1(
+    uint32_t key
+) {
     return (key * 31) % 64;
 }
 
 uint8_t 
-hash2(uint32_t key)
- {
+hash2(
+    uint32_t key
+) {
     return ((key >> 3) ^ (key * 17)) % 64;
 }
 
-bool 
-bloom_check(
-    uint32_t key
-)  {
-    bloom_filter[hash1(key) / 8] |= (1 << (hash1(key) % 8));
-    bloom_filter[hash2(key) / 8] |= (1 << (hash2(key) % 8));
+bool bloom_check(uint32_t key) {
+    return (bloom_filter[hash1(key) / 8] & (1 << (hash1(key) % 8))) &&
+           (bloom_filter[hash2(key) / 8] & (1 << (hash2(key) % 8)));
+}
+
+
+uint32_t 
+hash_packet_buffer(
+    uint8_t* buf, 
+    uint8_t size
+) {
+    uint32_t hash = 5381;
+    for (int i = 0; i < size; i++) {
+        hash = ((hash << 5) + hash) ^ buf[i]; // djb2 variant (fast and light)
+    }
+    return hash;
 }
 
 bool 
-bloom_check(
+bloom_check_packet(
+    uint8_t* packetBuffer,
+     uint8_t size
+) {
+    uint32_t key = hash_packet_buffer(packetBuffer, size); // or make_key_from_buffer
+    return bloom_check(key);
+}
+
+void bloom_add_packet(
+    uint8_t* packetBuffer,
+    uint8_t size
+) {
+    uint32_t key = hash_packet_buffer(packetBuffer, size); // or make_key_from_buffer
+    bloom_add(key);
+}
+
+void 
+bloom_add(
     uint32_t key
 ) {
-    return (bloom_filter[hash1(key) / 8] & (1 << (hash1(key) % 8))) &&
-           (bloom_filter[hash2(key) / 8] & (1 << (hash2(key) % 8)));
+    bloom_filter[hash1(key) / 8] |= (1 << (hash1(key) % 8));
+    bloom_filter[hash2(key) / 8] |= (1 << (hash2(key) % 8));
 }
